6d575199a22e0f3d0556bddfc56b6bd7634a8fae
RogaJess <97360917+RogaJess@users.noreply.github.com>
2023-08-21 21:57:30 -0600

    Finish game win/loss logic

--- rps.js
+++ rps.js
@@ -3,3 +3,17 @@ function getComputerChoice() {
 	const random = Math.floor(Math.random() * choices.length);
 	return choices[random].toLowerCase();
 }
+
+function playRound(playerSelection, computerSelection) {
+	if (playerSelection === computerSelection) {
+		console.log(`Tie Game! Both chose: ${playerSelection}!`);
+	} else if (
+		(playerSelection === "rock" && computerSelection === "scissors") ||
+		(playerSelection === "paper" && computerSelection === "rock") ||
+		(playerSelection === "scissors" && computerSelection === "paper")
+	) {
+		console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+	} else {
+		console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+	}
+}
